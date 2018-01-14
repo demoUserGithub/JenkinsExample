9f758f9f04e9c7bcfeb7bb3df7547760fe01d9fd
Unknown <2ov_1nvcphk347yo@byom.de>
2018-01-14 15:08:13 +0100

    Ready for testing

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,10 +1,11 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class ExampleTest {
+
+public class ExampleTest {
     @Test
-    void add() {
+    public void add() throws Exception {
         Example example = new Example();
         for(int i=0; i<10; i++)
             assertEquals(1+i, example.add(1,i));
